db9463dfc6538cd8f39ba382f78af89db6f3444e
Tamires Pereira <tamirespereira6@gmail.com>
2020-09-17 21:10:41 -0300

    Initial Commit

---
+++ FirebaseIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseIOS
+//
+//  Created by tamires.p.silva on 17/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
